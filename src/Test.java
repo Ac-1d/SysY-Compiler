4c79d5fac6b809f660619da647febda742f061c3
王玮宁 <22371548>
2024-09-29 14:36:16 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,71 @@
+import frontend.Config;
+import frontend.Lexer;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Config.setOriginalStream();
+        lexerTest();
+    }
+
+    private static boolean lexerTest() throws Exception{
+        Lexer instance = Lexer.getInstace();
+        String rootPath = "test_lexer";
+        String sourcePath = "/testfile.txt";
+        String lexerPath = "/lexer.txt";
+        String ansPath = "/ans.txt";
+        String resPath = "/res.txt";
+        for (DiffType type : DiffType.values()) {
+            String path = rootPath + "/" + type.toString();
+            File folder = new File(path);
+            File[] testCaseFoldersList = folder.listFiles();
+            for (File testCaseFolder : testCaseFoldersList) {
+                String testCasePath = testCaseFolder.getPath();
+                String source = Files.readString(Paths.get(testCasePath + sourcePath)) + "\n";
+                try {
+                    instance.lexerParse(source);
+                    System.setOut(new PrintStream(testCasePath + lexerPath));
+                    instance.printTokens();
+                    Scanner scLexer = new Scanner(new FileReader(testCasePath + lexerPath));
+                    Scanner scAns = new Scanner(new FileReader(testCasePath + ansPath));
+                    System.setOut(new PrintStream(testCasePath + resPath));
+                    // System.setOut(Config.originalStream);
+                    boolean success = true;
+                    while(scLexer.hasNextLine() && scAns.hasNextLine()) {
+                        String strLexer = scLexer.nextLine();
+                        String strAns = scAns.nextLine();
+                        if(strLexer.equals(strAns) == false) {
+                            System.out.println(strLexer + "|" + strAns);
+                            success = false;
+                        }
+                    }
+                    if(scLexer.hasNextLine() != scAns.hasNextLine()) {
+                        System.setOut(Config.originalStream);
+                        System.out.println(testCasePath + " ERROR");
+                    }
+                    else if(success) {
+                        System.out.println("Success!");
+                    }
+                    scLexer.close();
+                    scAns.close();
+                } catch (Exception e) {
+                    System.setOut(Config.originalStream);
+                    System.out.println(testCasePath + e.toString());
+                }
+
+            }
+        }
+        return true;
+    }
+
+    enum DiffType {
+        A,
+        B,
+        C,
+    };
+}
---
+++ src/frontend/Error.java
@@ -0,0 +1,16 @@
+package frontend;
+
+public class Error {
+    private String stage;
+    private int position;
+    private char errorCode;
+    Error(String stage, int position, char errorCode) {
+        this.stage = stage;
+        this.position = position;
+        this.errorCode = errorCode;
+    }
+    @Override
+    public String toString() {
+        return position + " " + Character.toString(errorCode);
+    }
+}
